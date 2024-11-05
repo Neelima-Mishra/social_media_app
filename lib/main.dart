aee74f05f2320cb114eae344ed99da7a32aa325b
NeelimaMishra-Fixity <neelima.m@fixitylearn.com>
2024-11-05 16:54:47 +0530

    init commit -1

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ import 'bloc/post/post_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+  );
   runApp(const MyApp());
 }
 
